bbe7474e1a7514b17f336cbc2a525aeb7b148440
Murari <murari@itresonance.com>
2026-05-03 17:41:05 +0530

    fix: simplify middleware logic by removing unnecessary redirect

--- middleware.js
+++ middleware.js
@@ -1,10 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  const { pathname } = request.nextUrl;
-  if (pathname !== '/') {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
+  return NextResponse.next();
 }
 
 export const config = {
